df9e1e069135faa09ddbe6c0340622da6197faa0
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2023-11-04 22:35:27 +0200

    Update Bigreal.cpp

--- Bigreal.cpp
+++ Bigreal.cpp
@@ -16,7 +16,7 @@ Bigreal::Bigreal(string& num){
                 number = num.substr(1, num.find('.')-1); }
             else
                 number = num.substr(0, num.find('.'));
-            if(num[0]=='+'){
+            if(num[0]=='+'||isdigit(num[0])){
                 real = num.substr(number.size() + 1, num.size() - 1); }
             else
                 real = num.substr(number.size() + 2, num.size() - 1);
@@ -35,10 +35,10 @@ Bigreal::Bigreal(string& num){
         real = "0";
     }
 }
-Bigreal Bigreal::operator+(Bigreal &b) {
-    string s;
-    Bigreal d(s);
+Bigreal Bigreal::operator+(Bigreal b) {
+    Bigreal d;
     if(sign == b.sign) {
+        d.sign = sign;
         if (number.size() > b.number.size()) {
             for (int i = 0; i < (int) number.size() - (int) b.number.size(); ++i) {
                 b.number.insert(0, "0");
@@ -71,7 +71,7 @@ Bigreal Bigreal::operator+(Bigreal &b) {
         int carry_real = 0, carry_integar = 0, carry_dot = 0;
         for (int i = (int) real1.size() - 1; i >= 0; --i) {
             if (real1[i] + real2[i] >= 10 && i == (int) real1.size() - 1) {
-                carry_real++;
+                carry_dot++;
                 result_real.push_front((real1[i] + real2[i]) % 10);
             } else if (real1[i] + real2[i] + carry_real >= 10) {
                 result_real.push_front((real1[i] + real2[i] + carry_real) % 10);
@@ -107,21 +107,16 @@ Bigreal Bigreal::operator+(Bigreal &b) {
                 }
             }
         }
-        output+=sign;
         for (int i: result_int) {
-            output += to_string(i);
+            d.number += to_string(i);
         }
-        output += '.';
         for (int i: result_real) {
-            output += to_string(i);
+            d.real += to_string(i);
         }
-        d.output = output;
-        d.number = number;
-        d.sign =sign;
-        d.ok = ok;
         return d;
     }
     else{
+        b.sign = (b.sign=='+'?'-':'+');
         return this->operator-(b);
     }
 }
@@ -168,12 +163,10 @@ bool Bigreal::operator==(Bigreal &b) {
         }
     }
     if(equal){
-        ok = true;
-        return ok;
+        return true;
     }
     else{
-        ok  = false;
-        return ok;
+        return false;
     }
 }
 bool Bigreal::operator<(Bigreal &b) {
@@ -197,36 +190,26 @@ bool Bigreal::operator<(Bigreal &b) {
             real.push_back('0');
         }
     }
-    deque<int> integar1, integar2, result_int, real1, real2, result_real;
-    for (int i = 0; i < number.size(); ++i) {
-        integar1.push_back(number[i] - '0');
-        integar2.push_back(b.number[i] - '0');
-    }
-    for (int i = 0; i < real.size(); ++i) {
-        real1.push_back(real[i] - '0');
-        real2.push_back(b.real[i] - '0');
-    }
-    bool smaller1 =true,equal = false;
-    for (int i = 0; i < integar1.size()&&smaller1; ++i) {
-        if(integar1[i]>integar2[i]){
-            smaller1 = false;
+    if(sign=='+'&&b.sign=='-'){
+        return false;
+    }else if(sign=='-'&&b.sign=='-'||sign=='+'&&b.sign=='+'){
+        if(number<b.number){
+            return true;
         }
-        if(integar1[i]==integar2[i]){
-            equal = true;
+        else if(number>b.number){
+            return false;
         }
-    }
-    for (int i = 0; i < real1.size()&&smaller1&&equal; ++i) {
-        if(integar1[i]>integar2[i]){
-            smaller1 = false;
+        else{
+            if(real<b.real){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-    }
-    if(smaller1){
-        ok = true;
-        return ok;
     }
     else{
-        ok  = false;
-        return ok;
+        return true;
     }
 }
 bool Bigreal::operator>(Bigreal &b) {
@@ -250,40 +233,31 @@ bool Bigreal::operator>(Bigreal &b) {
             real.push_back('0');
         }
     }
-    deque<int> integar1, integar2, result_int, real1, real2, result_real;
-    for (int i = 0; i < number.size(); ++i) {
-        integar1.push_back(number[i] - '0');
-        integar2.push_back(b.number[i] - '0');
-    }
-    for (int i = 0; i < real.size(); ++i) {
-        real1.push_back(real[i] - '0');
-        real2.push_back(b.real[i] - '0');
-    }
-    bool bigger1 =true,equal = false;
-    for (int i = 0; i < integar1.size()&&bigger1; ++i) {
-        if(integar1[i]<integar2[i]){
-            bigger1 = false;
+    if(sign=='-'&&b.sign=='+'){
+        return false;
+    }else if(sign=='-'&&b.sign=='-'||sign=='+'&&b.sign=='+'){
+        if(number>b.number){
+            return true;
         }
-        if(integar1[i]==integar2[i]){
-            equal = true;
+        else if(number<b.number){
+            return false;
         }
-    }
-    for (int i = 0; i < real1.size()&&bigger1&&equal; ++i) {
-        if(integar1[i]<integar2[i]){
-            bigger1 = false;
+        else{
+            if(real>b.real){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-    }
-    if(bigger1){
-        ok = true;
-        return ok;
     }
     else{
-        ok  = false;
-        return ok;
+        return true;
     }
 }
-Bigreal Bigreal::operator-(Bigreal &b) {
-    if(sign!=b.sign){
+Bigreal Bigreal::operator-(Bigreal b) {
+    Bigreal d;
+    if(sign==b.sign){
         if (number.size() > b.number.size()) {
             for (int i = 0; i < (int) number.size() - (int) b.number.size(); ++i) {
                 b.number.insert(0, "0");
@@ -313,23 +287,80 @@ Bigreal Bigreal::operator-(Bigreal &b) {
             real1.push_back(real[i] - '0');
             real2.push_back(b.real[i] - '0');
         }
+        int sub_carry = 0;
+        if(*this>b){
+            d.sign = sign;
+            for (int i = (int)real1.size()-1; i >=0 ; --i) {
+                if(real1[i]-real2[i]-sub_carry>=0){
+                    result_real.push_front(real1[i]-real2[i]-sub_carry);
+                    sub_carry = 0;
+                }
+                else if(real1[i]-real2[i]-sub_carry<0){
+                    result_real.push_front((real1[i]-real2[i]-sub_carry)+10);
+                    sub_carry = 1;
+                }
+            }
+            for (int i = (int)integar1.size()-1; i >=0 ; --i) {
+                if(integar1[i]-integar2[i]-sub_carry>=0){
+                    result_int.push_front(integar1[i]-integar2[i]-sub_carry);
+                    sub_carry = 0;
+                }
+                else if(integar1[i]-integar2[i]-sub_carry<0){
+                    result_real.push_front((integar1[i]-integar2[i]-sub_carry)+10);
+                    sub_carry = 1;
+                }
+            }
+        }
+        else{
+            d.sign = (b.sign=='+'?'-':'+');
+            for (int i = (int)real1.size()-1; i >=0 ; --i) {
+                if(real2[i]-real1[i]-sub_carry>=0){
+                    result_real.push_front(real2[i]-real1[i]-sub_carry);
+                    sub_carry = 0;
+                }
+                else if(real2[i]-real1[i]-sub_carry<0){
+                    result_real.push_front((real2[i]-real1[i]-sub_carry)+10);
+                    sub_carry = 1;
+                }
+            }
 
+            for (int i = (int)integar1.size()-1; i >=0 ; --i) {
+                if(integar2[i]-integar1[i]-sub_carry>=0){
+                    result_int.push_front(integar2[i]-integar1[i]-sub_carry);
+                    sub_carry = 0;
+                }
+                else if(integar2[i]-integar1[i]-sub_carry<0){
+                    result_int.push_front((integar2[i]-integar1[i]-sub_carry)+10);
+                    sub_carry = 1;
+                }
+            }
+        }
         for (int i: result_int) {
-            output += to_string(i);
+            d.number += to_string(i);
         }
-        output += '.';
         for (int i: result_real) {
-            output += to_string(i);
+            d.real += to_string(i);
         }
-        return output;
+        return d;
     }
     else{
+        b.sign = (b.sign=='+'?'-':'+');
         return this->operator+(b);
     }
 }
-void Bigreal::print_number_sign_real(){
-    cout<<sign<<number<<"."<<real<<'\n';
+ostream &operator<<(ostream &out, const Bigreal& num){
+    out<<num.sign<<num.number<<"."<<num.real<<'\n';
+    return out;
 }
-void Bigreal::print_result() {
-    cout<<"The result of the sum is: \n"<<output;
+int Bigreal::size(){
+    return (int)number.size()+(int)real.size()+1;
+}
+char Bigreal::sign_of_Bigreal() const{
+    return sign;
+}
+Bigreal Bigreal::set_values() {
+    string s;
+    cin>>s;
+    Bigreal a(s);
+    return a;
 }
