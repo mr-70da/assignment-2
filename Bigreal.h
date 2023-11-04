0c48e1cd340427a14e0c323f17131cca68cd7dec
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2023-11-04 22:35:45 +0200

    Update Bigreal.h

--- Bigreal.h
+++ Bigreal.h
@@ -1,7 +1,3 @@
-//
-// Created by acer on 11/3/2023.
-//
-
 #ifndef UNTITLED1_BIGREAL_H
 #define UNTITLED1_BIGREAL_H
 #include <string>
@@ -11,16 +7,21 @@ using namespace std;
 
 class Bigreal {
     char sign = '+';
-    bool ok = true;
-    string number,real,output;
+    string number,real;
 public:
-    Bigreal(string& number);
-    Bigreal operator+(Bigreal& b);
-    Bigreal operator-(Bigreal& b);
+    Bigreal(){
+        sign = '+';
+        number = "",real = "";
+    };
+    int size();
+    [[nodiscard]] char sign_of_Bigreal() const;
+    explicit Bigreal(string& number);
+    static Bigreal set_values();
+    Bigreal operator+(Bigreal b);
+    Bigreal operator-(Bigreal b);
     bool operator>(Bigreal& b);
     bool operator==(Bigreal& b);
     bool operator<(Bigreal& b);
-    void print_number_sign_real();
-    void print_result();
+    friend ostream &operator<<(ostream &out, const Bigreal& num);
 };
 #endif //UNTITLED1_BIGREAL_H
